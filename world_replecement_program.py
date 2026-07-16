60d1b44378580438237dcacd20db870542233040
navin420-xy <naveenbairwa147@gmail.com>
2026-07-16 17:38:14 +0530

    new_file

---
+++ world_replecement_program.py
@@ -0,0 +1,8 @@
+def raplace_world():
+
+    str='HI EVERYONE,I AM NAVEEN'
+    world_to_replece=input("ENTER THE WORLD REPLACE ")
+    world_replecement=input("ENTER THE WORLD REPLECEMENT")
+    print(str.replace(world_to_replece,world_replecement))
+
+raplace_world()
